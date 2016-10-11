c0aa236674edda8d30404d20ce83f0a6b1d2a2eb
Devin Thomas <devin.w.thomas@gmail.com>
2016-10-11 12:33:50 -0400

    added some comments and profiled, for line in file: is a few percent faster than readlines() and there is no obvious source of any large speedups.

--- src/taxa_callback.py
+++ src/taxa_callback.py
@@ -19,41 +19,55 @@ def get_uniprot_id(ec, filename):
         return acc
     
 def uniprot_coords(uniprot_id, filename):
+    """
+    Pull the header for the contig file from the .sam output file of paladin
+    uniprot_id: a uniprot_id (str)
+    filename: path to sam file (str)
+    """
     lines = []
     index = 0
     with open(filename) as f:
         for line in f:
-            if not line[0] == '@':
+            if not line[0] == '@':  ## if not a header
                 line = line.split()
-                if uniprot_id in line[2]:
-                    lines.append(line[0].split(':')[-1])
+                if uniprot_id in line[2]: ## and matching the uniprot_id
+                    lines.append(line[0].split(':')[-1])  ## append the contig header
     return lines
 
+
 def find_seq(loh, filename):
-    """ 
-    Return a list of seqeunces from fastq file (filename) corresponding 
+    """                                                                                                            
+    Return a list of seqeunces from fastq file (filename) corresponding
     to the headers in loh
+    loh: list of headers (list(string))
+    filename: contig.fasta file to search for the list of headers of                              
+    Returns:                                                                                                  
+    los: dictionary with the contig headers as keys and sequences as values     
     """
     flag = False
     header = ''
     los = {}
     with open(filename) as f:
-        lines = f.readlines(100000)
-        while lines:
-            for line in lines:
-                if flag:
-                    los[header] = line.rstrip()
-                    flag = False
-                    header = ''
-                else:
-                    if line[0] == '@':
-                        if line[1:-3] in loh:
-                            header = line.rstrip()
-                            flag = True
-            lines = f.readlines(100000)
+        for line in f:
+            if flag:
+                los[header] = line.rstrip()
+                flag = False
+                header = ''
+            else:
+                if line[0] == '@':
+                    if line[1:-3] in loh:
+                        header = line.rstrip()
+                        flag = True
     return los
+
         
 if __name__== "__main__":
-   uid = get_uniprot_id('1.4.1.13', '/home/work/Documents/pathways/paladin-pathways/Pathways_Output/B.japonicum_100_test.csv')
-   loh = uniprot_coords(uid[0], '/home/work/Documents/pathways/paladin-pathways/Paladin_Output/B.japonicum_100.sam')
-   los = find_seq(loh, "../B.japonicum_100.1.fq")
+    import time
+    uid = get_uniprot_id('1.4.1.13', '/home/work/Documents/pathways/paladin-pathways/Pathways_Output/B.japonicum_100_test.csv')
+    loh = uniprot_coords(uid[0], '/home/work/Documents/pathways/paladin-pathways/Paladin_Output/B.japonicum_100.sam')
+    st1 = time.time()
+    los = find_seq(loh, "../B.japonicum_100.1.fq")
+    print(time.time()-st1)
+    st1 = time.time()
+    los2 = find_seq2(loh, "../B.japonicum_100.1.fq")
+    print(time.time()-st1)
