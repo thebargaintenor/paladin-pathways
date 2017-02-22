aa12fc1367029a96970c81f2c0d6d7b3373272e2
Devin Thomas <devin.w.thomas@gmail.com>
2017-02-22 14:16:20 -0500

    incorperated kegg into pathways_plugin.py

--- src/pathways_plugin.py
+++ src/pathways_plugin.py
@@ -8,7 +8,10 @@ import csv
 import paladin_postprocess
 import kegg
 import requests
-
+import dataset
+import xmltodict
+import os
+import sys
 '''
 pathways.py
 
@@ -112,6 +115,134 @@ def group(lst, n):
         if len(val) == n or len(val) == len(lst):
             yield val
 
+"""
+KEGG
+"""
+
+def kegg_get(pathway_id, overwrite=False):
+    '''Load KGML from either the local database or KEGG
+    (caching the new KGML)'''
+    path = os.path.dirname(__file__)
+    # I stubbornly insist on continued windows compatibility
+    if sys.platform == 'win32':
+        path += '\\'
+    else:
+        path += '/'
+    # database is stored in folder with scripts so that changing the working
+    # directory doesn't require a new cache
+    # (plus it can be shared with multiple users)
+    db = dataset.connect('sqlite:///' + path + 'kgmlcache.db')
+    kgml_table = db.get_table('kgml')
+
+    if not kgml_table:
+        record = download_kgml(pathway_id)
+        kgml_table.insert(record, ['id'])
+        kgml_table.create_index(['id'], 'pathway_id')
+        kgml = record['kgml']
+    elif overwrite:
+        record = download_kgml(pathway_id)
+        kgml_table.upsert(record, ['id'])
+        kgml = record['kgml']
+    else:
+        record = kgml_table.find_one(id=pathway_id)
+        if record:
+            kgml = record['kgml']
+        else:
+            record = download_kgml(pathway_id)
+            kgml_table.insert(record)
+            kgml = record['kgml']
+
+    if kgml:
+        return json.loads(kgml)  # return none if no pathway json found
+
+
+def download_kgml(pathway_id):
+    '''
+    Download KGML for the pathway and return a record ready for DB storage
+    '''
+    # but really, get the KGML, build dictionary from it, add data from
+    # KEGG dat file (which has different stuff in it, because reasons)
+    # I need that data stuffed into my JSON for the visualization, as
+    # the regular KGML has no names for the compounds in the chart
+
+    url = 'http://rest.kegg.jp/get/ec' + pathway_id
+    plugins.core.sendOutput(": ".join(['Retrieving data from', url]), 'stdout')
+    r = requests.kegg_get(url)
+
+    # retrieve DAT version of pathway first (Because.)
+    datfile = None
+    if r.status_code == 200:  # 200 is no error, so proceed happily
+        datfile = r.text
+        if not datfile:
+            plugins.core.sendOutput(" ".join(['No data for pathway',
+                                              pathway_id,
+                                              'found on KEGG.']), 'stdout')
+    else:
+        plugins.core.sendOutput(" ".join(['HTTP Error',
+                                          r.status_code]) +
+                                "\nDownload process halting", "stderr")
+        return  # don't bother requesting anything else
+
+    url += '/kgml'
+    plugins.core.sendOutput(" ".join(['Retrieving data from ', url]), "stdout")
+    r = requests.kegg_get(url)
+
+    # retrieve KGML of pathway
+    kgml = None
+    if r.status_code == 200:  # 200 is no error, so proceed happily
+        kgml = r.text
+        if not kgml:
+            plugins.core.sendOutput(" ".join(['No data for pathway',
+                                              pathway_id,
+                                              'found on KEGG.']), "stdout")
+    else:
+        plugins.core.sendOutput(" " .join(['HTTP Error',
+                                           r.status_code]) +
+                                'Download process halting', "stdout")
+        return  # don't bother requesting anything else
+
+    # convert KGML to dictionary for JSON output,
+    # but add a dictionary mapping KEGG IDs to compound names
+    kgml_dict = xmltodict.parse(kgml)
+
+    # extract compounds and add them to dictionary
+    if kgml_dict['pathway']:
+        kgml_dict['pathway']['compounds'] = extract_compounds(datfile)
+
+    # create database record from amended dictionary
+    kgml = json.dumps(kgml_dict, indent=4)
+    record = dict(id=pathway_id,
+                  name=kgml_dict['pathway']['@title'],
+                  kgml=kgml)
+    return record
+
+
+def extract_compounds(data):
+    '''
+    build a dictionary of compound IDs and names from KEGG pathway DAT file
+    '''
+    compounds = {}
+    in_compound_section = False
+    for line in data.split('\n'):
+        section = line[:12].rstrip()
+        if section == "COMPOUND":
+            in_compound_section = True
+        elif section and section != "COMPOUND":
+            in_compound_section = False
+
+        # extract compound information from record
+        if in_compound_section and line:
+            cid = line[12:20].rstrip()
+            cname = line[20:].rstrip()
+            compounds[cid] = cname
+
+    return compounds
+
+
+"""
+MAIN
+"""
+
 
 def main_pathways(arguments):
     # Parse arguments
@@ -137,7 +268,7 @@ def main_pathways(arguments):
                            help='verbose mode',
                            action='store_true',
                            dest="verbose")
-    arguments = vars(argParser.parse_args(arguments))
+    arguments = vars(argParser.parse_known_args(arguments)[0])
     kegg_id = arguments["kegg"]
     paladin_report = arguments["paladin"]
     output_csv = arguments["output"]
@@ -149,7 +280,7 @@ def main_pathways(arguments):
         kegg_id, verbose)
 
     # retrieve dictionary of pathway information
-    pathway = kegg.get(kegg_id)
+    pathway = kegg_get(kegg_id)
 
     if pathway:
         pathway = pathway['pathway']
@@ -173,7 +304,7 @@ def main_pathways(arguments):
                 # assume ALL listed refs are needed when listed in @name
                 ecs = [ec[3:] for ec in entry['@name'].split(' ')]
                 for ec in ecs:
-                    if not ec in enzyme_names:
+                    if ec not in enzyme_names:
                         enzymes.append(ec)
                         enzyme_names[ec] = ''
                         enzyme_counts[ec] = 0
@@ -189,16 +320,24 @@ def main_pathways(arguments):
         with open(filtered_paladin_csv, 'r') as paladin_file:
             paladin_records = csv.reader(paladin_file, delimiter=',')
 
-            # open output file for immediate writing in same pass as reading paladin report
+            # open output file for immediate writing
+            # in same pass as reading paladin report
             with open(output_csv, 'w') as csvfile:
-                writer = csv.writer(csvfile, quoting=csv.QUOTE_MINIMAL, lineterminator='\n')
-                writer.writerow(['uniprot', 'brenda', 'gene_name', 'organism', 'count', 'abundance'])
+                writer = csv.writer(csvfile,
+                                    quoting=csv.QUOTE_MINIMAL,
+                                    lineterminator='\n')
+                writer.writerow(['uniprot',
+                                 'brenda',
+                                 'gene_name',
+                                 'organism',
+                                 'count',
+                                 'abundance'])
 
                 # iterate over incoming report
                 for row in paladin_records:
                     matches = tuple(e for e in enzymes if is_match(e, row[1]))
-
-                    # I don't want to calculate matches twice, so incur memory impact of tuple
+                    # I don't want to calculate matches twice,
+                    # so incur memory impact of tuple
                     if len(matches) > 0:
                         writer.writerow(row)  # gib whole row plox.
                         for match in matches:  # there could be more than one given wildcards
